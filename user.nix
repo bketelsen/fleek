df160f213060ff1063417f816fda8f8abf37dfca
Brian Ketelsen <bketelsen@gmail.com>
2023-04-26 19:11:24 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,6 +1,8 @@
 { pkgs, misc, ... }: {
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
-
+home.sessionVariables = {
+  FLEEK_MANAGED= "1";
+};
  
 }
