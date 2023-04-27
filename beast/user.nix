1e52b098319b1dcc4bfef0efef126a6c83bd2920
Brian Ketelsen <bketelsen@gmail.com>
2023-04-27 09:50:07 -0400

    fleek: apply

--- beast/user.nix
+++ beast/user.nix
@@ -2,5 +2,6 @@
   # This file will never be modified by fleek
   # configs mentioned here must be listed in ~/fleek.yml #programs array or you will get errors
   programs.librewolf.enable = true;
+  targets.genericLinux.enable = true;
  
 }
