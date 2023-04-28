9ed0b2ec7ca51b2c45978f396abd0f56f7ecd9a1
Brian Ketelsen <bketelsen@gmail.com>
2023-04-28 10:51:17 -0400

    fleek: apply

--- user.nix
+++ user.nix
@@ -10,5 +10,21 @@ services.syncthing.enable = true;
 services.syncthing.extraOptions = [
   "--gui-address=http://0.0.0.0:8384"
 ];
- 
+
+
+  getAstroNvim = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    $DRY_RUN_CMD rm -rf $VERBOSE_ARG \
+        $HOME/.config/nvim
+    $DRY_RUN_CMD rm -rf $VERBOSE_ARG \
+        $HOME/.local/share/nvim
+    $DRY_RUN_CMD rm -rf $VERBOSE_ARG \
+        $HOME/.local/state/nvim
+    $DRY_RUN_CMD rm -rf $VERBOSE_ARG \
+        $HOME/.cache/nvim
+    $DRY_RUN_CMD git clone --depth 1 \
+      https://github.com/AstroNvim/AstroNvim \
+      ~/.config/nvim
+    
+  '';
+
 }
