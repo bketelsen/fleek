77cb10ecd9663566e914aa3c2bc49428c2a81dd9
Brian Ketelsen <bketelsen@gmail.com>
2023-04-30 11:33:31 -0400

    join new system

--- flake.nix
+++ flake.nix
@@ -68,6 +68,30 @@
         ];
       };
       
+      "bjk@chapterhouse" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+
+        modules = [ 
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./chapterhouse/chapterhouse.nix
+          ./chapterhouse/user.nix
+          # self-manage fleek
+          {
+           home.packages = [
+            fleek.packages.aarch64-darwin.default
+          ];
+          }
+
+        ];
+      };
+      
     };
   };
 }
