5f4beda47778322ed381240df1a68313b28c0cb1
Brian Ketelsen <bketelsen@gmail.com>
2023-09-20 16:09:46 -0400

    join new system

--- flake.nix
+++ flake.nix
@@ -22,6 +22,8 @@
     
      packages.aarch64-darwin.fleek = fleek.packages.aarch64-darwin.default;
     
+     packages.x86_64-linux.fleek = fleek.packages.x86_64-linux.default;
+    
     # Available through 'home-manager --flake .#your-username@your-hostname'
     
     homeConfigurations = {
@@ -43,6 +45,32 @@
           {
             home.packages = [
               fleek.packages.aarch64-darwin.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
+      "bjk@ghanima" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./ghanima/bjk.nix
+          ./ghanima/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
             ];
           }
           ({
