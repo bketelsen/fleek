32bea69444773ef472b5bdbdbecca6b8ded2491f
bjk <bketelsen@gmail.com>
2023-09-21 14:17:52 +0000

    join new system

--- flake.nix
+++ flake.nix
@@ -106,6 +106,32 @@
         ];
       };
       
+      "debian@debiandev" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+        modules = [
+          ./home.nix 
+          ./path.nix
+          ./shell.nix
+          ./user.nix
+          ./aliases.nix
+          ./programs.nix
+          # Host Specific configs
+          ./debiandev/debian.nix
+          ./debiandev/custom.nix
+          # self-manage fleek
+          {
+            home.packages = [
+              fleek.packages.x86_64-linux.default
+            ];
+          }
+          ({
+           nixpkgs.overlays = [];
+          })
+
+        ];
+      };
+      
     };
   };
 }
